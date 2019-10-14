8573ea87ae073da6e1397634458157cbeea0a3c9
William White <william.white86@gmail.com>
2019-10-14 12:01:09 -0400

    wrapped question setting in function to allow incrementing based on click events

--- script.js
+++ script.js
@@ -1,84 +1,92 @@
 
-document.getElementById("question").innerHTML = questions[0]["title"];
-document.getElementById("answerOne").innerHTML = questions[0]["choices"][0];
-document.getElementById("answerTwo").innerHTML = questions[0]["choices"][1];
-document.getElementById("answerThree").innerHTML = questions[0]["choices"][2];
-document.getElementById("answerFour").innerHTML = questions[0]["choices"][3];
+// document.getElementById("question").innerHTML = questions[0]["title"];
+// document.getElementById("answerOne").innerHTML = questions[0]["choices"][0];
+// document.getElementById("answerTwo").innerHTML = questions[0]["choices"][1];
+// document.getElementById("answerThree").innerHTML = questions[0]["choices"][2];
+// document.getElementById("answerFour").innerHTML = questions[0]["choices"][3];
+
+// document.getElementById("answerOne").addEventListener("click", function(){
+//     if (questions[0]["choices"][0] === questions[0]["answer"]){
+//         alert("correct");}
+//     else {
+//         alert("wrong");
+//     }
+// })
+
+// document.getElementById("answerTwo").addEventListener("click", function(){
+//     if (questions[0]["choices"][1] === questions[0]["answer"]){
+//         alert("correct");}
+//     else {
+//         alert("wrong");
+//     }
+// })
+
+// document.getElementById("answerThree").addEventListener("click", function(){
+//     if (questions[0]["choices"][2] === questions[0]["answer"]){
+//         alert("correct");}
+//     else {
+//         alert("wrong");
+//     }
+// })
+
+// document.getElementById("answerFour").addEventListener("click", function(){
+//     if (questions[0]["choices"][3] === questions[0]["answer"]){
+//         alert("correct");}
+//     else {
+//         alert("wrong");
+//     }
+// })
+    
+var i = 0;
+
+function questionSetter(){
+    document.getElementById("question").textContent = questions[i]["title"];
+    document.getElementById("answerOne").textContent = questions[i]["choices"][0];
+    document.getElementById("answerTwo").textContent = questions[i]["choices"][1];
+    document.getElementById("answerThree").textContent = questions[i]["choices"][2];
+    document.getElementById("answerFour").textContent = questions[i]["choices"][3];}
+
+questionSetter();
 
 document.getElementById("answerOne").addEventListener("click", function(){
-    if (questions[0]["choices"][0] === questions[0]["answer"]){
+    if (questions[i]["choices"][0] === questions[i]["answer"]){
         alert("correct");}
     else {
         alert("wrong");
     }
+    i++;
+    questionSetter();
 })
 
 document.getElementById("answerTwo").addEventListener("click", function(){
-    if (questions[0]["choices"][1] === questions[0]["answer"]){
-        alert("correct");}
+    if (questions[i]["choices"][1] === questions[i]["answer"]){
+        alert("correct");
+        }
     else {
         alert("wrong");
     }
+    i++;
+    questionSetter();
 })
 
 document.getElementById("answerThree").addEventListener("click", function(){
-    if (questions[0]["choices"][2] === questions[0]["answer"]){
+    if (questions[i]["choices"][2] === questions[i]["answer"]){
         alert("correct");}
     else {
         alert("wrong");
     }
+    i++;
+    questionSetter();
 })
 
 document.getElementById("answerFour").addEventListener("click", function(){
-    if (questions[0]["choices"][3] === questions[0]["answer"]){
+    if (questions[i]["choices"][3] === questions[i]["answer"]){
         alert("correct");}
     else {
         alert("wrong");
     }
+    i++;
+    questionSetter();
 })
 
 
-document.getElementById("answers").addEventListener("click", function(){
-
-    var i = 1;
-
-    document.getElementById("question").innerHTML = questions[i]["title"];
-    document.getElementById("answerOne").innerHTML = questions[i]["choices"][0];
-    document.getElementById("answerTwo").innerHTML = questions[i]["choices"][1];
-    document.getElementById("answerThree").innerHTML = questions[i]["choices"][2];
-    document.getElementById("answerFour").innerHTML = questions[i]["choices"][3];
-
-    document.getElementById("answerOne").addEventListener("click", function(){
-        if (questions[i]["choices"][0] === questions[i]["answer"]){
-            alert("correct");}
-        else {
-            alert("wrong");
-        }
-    })
-
-    document.getElementById("answerTwo").addEventListener("click", function(){
-        console.log(i);
-        if (questions[i]["choices"][1] === questions[i]["answer"]){
-            alert("correct");
-            }
-        else {
-            alert("wrong");
-        }
-    })
-
-    document.getElementById("answerThree").addEventListener("click", function(){
-        if (questions[i]["choices"][2] === questions[i]["answer"]){
-            alert("correct");}
-        else {
-            alert("wrong");
-        }
-    })
-
-    document.getElementById("answerFour").addEventListener("click", function(){
-        if (questions[i]["choices"][3] === questions[i]["answer"]){
-            alert("correct");}
-        else {
-            alert("wrong");
-        }
-    })
-})
