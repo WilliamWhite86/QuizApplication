6e610ddda397c322c31a1daf5afea37c1345e089
William White <william.white86@gmail.com>
2019-10-13 23:45:57 -0400

    added logic to switch to 2nd question, working logic to validate 2nd question.

--- questions.js
+++ questions.js
@@ -1,9 +1,14 @@
 
 
 var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    }]
-  
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "question 2",
+    choices: ["a", "b", "c", "d"],
+    answer: "b"
+  }
+]
--- script.js
+++ script.js
@@ -36,3 +36,49 @@ document.getElementById("answerFour").addEventListener("click", function(){
         alert("wrong");
     }
 })
+
+
+document.getElementById("answers").addEventListener("click", function(){
+
+    var i = 1;
+
+    document.getElementById("question").innerHTML = questions[i]["title"];
+    document.getElementById("answerOne").innerHTML = questions[i]["choices"][0];
+    document.getElementById("answerTwo").innerHTML = questions[i]["choices"][1];
+    document.getElementById("answerThree").innerHTML = questions[i]["choices"][2];
+    document.getElementById("answerFour").innerHTML = questions[i]["choices"][3];
+
+    document.getElementById("answerOne").addEventListener("click", function(){
+        if (questions[i]["choices"][0] === questions[i]["answer"]){
+            alert("correct");}
+        else {
+            alert("wrong");
+        }
+    })
+
+    document.getElementById("answerTwo").addEventListener("click", function(){
+        console.log(i);
+        if (questions[i]["choices"][1] === questions[i]["answer"]){
+            alert("correct");
+            }
+        else {
+            alert("wrong");
+        }
+    })
+
+    document.getElementById("answerThree").addEventListener("click", function(){
+        if (questions[i]["choices"][2] === questions[i]["answer"]){
+            alert("correct");}
+        else {
+            alert("wrong");
+        }
+    })
+
+    document.getElementById("answerFour").addEventListener("click", function(){
+        if (questions[i]["choices"][3] === questions[i]["answer"]){
+            alert("correct");}
+        else {
+            alert("wrong");
+        }
+    })
+})
