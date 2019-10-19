91e2f71d139c4e9d4b90c8d39d994b7fecd6d6be
William White <william.white86@gmail.com>
2019-10-18 23:44:12 -0400

    updated renderscore function to render score objects on table, then sorted scores from high to low

--- scoreBoard.js
+++ scoreBoard.js
@@ -1,7 +1,5 @@
 var highScoreList = document.querySelector("#highScores");
 
-console.log(highScoreList);
-
 initScores();
 function initScores() {
     storedScores = JSON.parse(localStorage.getItem("scores"));
@@ -15,14 +13,31 @@ function initScores() {
 function renderScores() {
     //highScoreList.innerHTML = "";
     // Render new li for each highscore
+    scoreList.sort(function(a,b){
+        return a.newScore - b.newScore;
+    });
+    scoreList.reverse(function(a,b){
+        return a.newScore - b.newScore
+    })
     console.log(scoreList);
     for (var i = 0; i < scoreList.length; i++) {
         var scoreListItem = scoreList[i];
 
-        var li = document.createElement("li");
-        li.textContent = scoreListItem;
-        console.log(scoreListItem);
-        //li.setAttribute("data-index", i);
-        highScoreList.appendChild(li);
+        var tr = document.createElement("tr");
+        tr.setAttribute("tr-index", i);
+        document.getElementById("highScores").appendChild(tr);
+
+        var nameCell = document.createElement("td");
+        tr.appendChild(nameCell);
+
+        var nameCellText = document.createTextNode(scoreListItem.name);
+        nameCell.appendChild(nameCellText);
+
+        var scoreCell = document.createElement("td");
+        tr.appendChild(scoreCell);
+
+        var scoreCellNum = document.createTextNode(scoreListItem.newScore);
+        tr.appendChild(scoreCellNum);
+
     }
 }
